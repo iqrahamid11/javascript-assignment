656f9f07d4cd7836fefad6276191ad1faa3be088
iqra hamid <hamidiqra55@gmail.com>
2021-06-19 11:18:43 -0700

    Work Accomplish

---
+++ app.js
@@ -0,0 +1,607 @@
+/*// CHAPTER # 1
+
+// Question no 1 by Chapter 1
+
+alert ("Hello World!");
+
+// Question no 2 by Chapter 1
+
+alert ("Error! Please enter a valid password.");
+
+// Question no 3 by Chapter 1
+
+alert ("Welcome to JS Land... \nHappy Coding!");
+
+// Question no 4 by Chapter 1
+
+alert ("Welcome to JS Land...");
+
+alert ("Happy Coding! \nPrevent this page from creating additional dialogs."); 
+
+// Question no 5 by chapter 1
+
+alert("Hello... I can run JS through my web brower's console");*/
+
+// Question no 6 by Chapter 1
+
+// Question no 7 by Chapter 1
+
+
+/*// chapter # 2
+
+// Question no 1 by Chapter 2 (variables for string)
+
+var username = "john doe";
+
+// Question no 2 by Chapter 2 
+
+var myName = "Iqra Hamid";
+
+// Question no 3 by Chapter 2 
+
+var message = "Hello World";
+alert(message);
+
+// Question no 4 by Chapter 2 
+
+var name = "jhone doe";
+alert(name);
+
+var age = "15 years old";
+alert(age);
+
+var certify = "Certified Mobile Application Development";
+alert(certify)
+
+// Question no 5 by Chapter 2 
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza)
+
+// Question no 6 by Chapter 2 
+
+var email = "hamidiqra55@gmail.com";
+alert(email);
+
+// Question no 7 by Chapter 2 
+
+var book = "I am trying to learn fron the Book A smater way to learn JavaScript";
+alert(book);
+
+// Question no 8 by Chapter 2 
+
+var a = "Yah! I can write HTML content through JavaScript";
+document.write(a);
+
+// Question no 9 by Chapter 2 
+
+alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");*/
+
+
+/*// CHAPTER # 3  (variables for numbers)
+
+// Question no 1 by Chapter 3 
+
+var age = "I am 15 years old";
+alert (age);
+
+// Question no 2 by Chapter 3
+
+var visited = "You have visited this site 14 times";
+alert (visited);
+
+// Question no 3 by Chapter 3
+
+var birthYear = ("My birth year is 1990 <br/> Data type of my ddeclared variable is number");
+document.write (birthYear);
+
+// Question no 4 by Chapter 3
+
+var visitorName = "John Doe";
+var quantity = " ordered 5 T-shirt(s)";
+var store = " on XYZ Clothing store";
+
+alert (visitorName+quantity+store);*/
+
+
+/*// CHAPTER # 4 LEGAL AND ILLEGAL
+
+// Question no 1 by Chapter 4
+
+//  chap 4( VARIABLES NAMES:LEGAL & ILLEGAL) 
+
+//  Rules for naming JS variables 
+
+
+var number= ("Variable names can only contain,numbers, $ and _ . For example :$my_1st variable");
+var letter= ("Variable must begin with a letter, $ or _. For example:$name,_name or name");
+var name= ("Variable name are case sentitive");
+var keywords= ("Variable names should not be JS keywords");
+
+document.write ("Variable names can only contain,numbers, $ and _ . 
+For example :$my_1st variable <br/>Variable must begin with a letter,
+ $ or _. For example:$name,_name or name <br/>Variable name are case 
+sentitive <br/>Variable names should not be JS keywords")*/
+
+
+//  CHAP 5 (math expressions) 
+
+// Q1
+
+
+// var a= 3;
+// var b= 5;
+// var addition= (a+b);
+
+// document.write("Sum of 3 and 5 is 8");
+
+
+
+// //  Q2 
+
+
+//     var a= 3;
+//     var b= 5;
+//     var subraction= (b-a);
+
+//     document.write ("Sub of 5 and 3 is 2");
+
+
+
+
+//     var a= 3;
+//     var b= 5;
+//     var multiplication= (a*b)
+
+//     document.write ("Multiply of 3 and 5 is 15")
+
+
+
+// chap 9-11(USER INPUT & CONDITIONAL STATEMENT)
+//  QNO1
+
+// var city= prompt ("Enter your city name");
+// if(city ==karachi) {
+//     alert("Welcome to city of lights")
+// }
+
+// QNO2
+
+// var gender= +prompt ("Gender")
+
+// if (gender==male){
+
+//    alert(" Good Morning Sir.")
+// }
+
+// else if(gender==female){
+
+// alert ("God Morning Ma'am. ")
+// }
+
+
+
+// QNO3
+
+// var a= +prompt ("road traffic signals");
+
+// if (a== red){
+
+//     alert("Must Stop")
+// }
+// else if (a==yellow){
+
+//     alert("Ready to move")
+// }
+
+// else if (a==green){
+
+//     alert ("Move now")
+// }
+
+
+
+// chapter 14-16
+
+
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+// var studentsName = [];
+
+// 2. Declare an empty array using JS object notation to store student names in future.
+
+// var a = {};
+
+// 3. Declare and initialize a strings array.
+
+// var animals= ["cat", "monkey", "rabbit", "lion"];
+// document.write(animals);
+
+// 4. Declare and initialize a numbers array.
+
+// var numbers = [10,20,30,40,50];
+// document.write(numbers);
+
+// 5. Declare and initialize a boolean array.
+
+
+
+// 6. Declare and initialize a mixed array.
+
+// var arr = [10 , "cat" , "true" ];
+// document.write(arr);
+
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+
+// var array = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil.", "PhD"];
+
+// document.write("<ol>");
+// for(a = 0; a <=7; a++){
+//     document.write("<li>" + array[a] + "</li>");
+// }
+
+// document.write("</ol>");
+
+/*8. Write a program to store 3 student names in an array.Take
+another array to store score of these three students.
+Assume that total marks are 500 for each student, display
+the scores & percentages of students like:*/
+
+/*var ary = [
+    ["Michael", "320. ", "64%"],
+    ["John", "230. ", "46%"],
+    ["Tony", "480. ", "96%"]
+];
+for(var a = 0; a < 1; a++){
+    
+    document.write("Score of " + ary[0][0] + " is " + ary[0][1] + "Percentage: " + ary[0][2] + "<br>");
+    document.write ("Score of " + ary[1][0] + " is " + ary[1][1] + "Percentage: " + ary[1][2] + "<br>");
+    document.write ("Score of " + ary[2][0] + " is " + ary[2][1] + "Percentage: " + ary[2][2]);
+}*/
+
+/*10. Write a program to store student scores in an array &
+sort the array in ascending order using Array’s sort
+method.*/
+
+/*var scores = [320,230,480,120];
+document.write("Scores of Students: " + scores + "<br>");
+scores.sort();
+document.write("Ordered Scores of Students: " + scores);*/
+
+
+/*11. Write a program to initialize an array with city names.
+Copy 3 array elements from cities array to selectedCities
+array.*/
+
+/*var cityNames = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+
+document.write("Cities list:" +"<br>"+ cityNames + "<br>" + "<br>");
+
+var selectedCity = cityNames.slice(2,4);
+document.write("Selected cities list:" +"<br>"+ selectedCity);*/
+
+
+/*12. Write a program to create a single string from the
+below mentioned array:
+var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+(Use array’s join method)*/
+
+/*var arr = ["This", "is", "my", "cat"];
+document.write(arr + "<br>");
+
+var array = arr.join(" ");
+document.write(array)*/
+
+/*13. Create a new array. Store values one by one in such a way
+that you can access the values in the order in which they
+were stored. (FIFO-First In First Out)*/
+
+
+/*var Devices = ["keyboard", "mouse", "printer", "moniter"];
+
+document.write("devices:" + "<br>" + Devices + "<br><br>");
+
+var Out = Devices.slice(0,1);
+document.write("Out:" +"<br>" + Out +"<br>");
+
+var Out = Devices.slice(1,2);
+document.write("Out:" +"<br>" + Out +"<br>");
+
+var Out = Devices.slice(2,3);
+document.write("Out:" +"<br>" + Out +"<br>");
+
+var Out = Devices.slice(3,4);
+document.write("Out:" +"<br>" + Out +"<br>");*/
+
+
+/*14. Create a new array. Store values one by one in such a way
+
+that you can access the values in reverse order. (Last In-
+First Out)*/
+
+
+/*var Devices = ["keyboard", "mouse", "printer", "moniter"];
+
+document.write("devices:" + "<br>" + Devices + "<br><br>");
+
+var Out = Devices.slice(3,4);
+document.write("Out:" +"<br>" + Out +"<br>");
+
+var Out = Devices.slice(2,3);
+document.write("Out:" +"<br>" + Out +"<br>");
+
+var Out = Devices.slice(1,2);
+document.write("Out:" +"<br>" + Out +"<br>");
+
+var Out = Devices.slice(0,1);
+document.write("Out:" +"<br>" + Out +"<br>");*/
+
+
+
+/*15. Write a program to store phone manufacturers (Apple,
+    Samsung, Motorola, Nokia, Sony & Haier) in an array.
+    Display the following dropdown/select menu in your
+    browser using document.write() method:*/
+
+
+// var phone = ["Apple", "Samsung", "Motorola", "Nokia", "Sony & Haier"];
+
+// document.write(phone +  "<br>")
+
+
+
+// arrays and loops  17 - 20 chap
+
+/*1. Declare and initialize an empty multidimensional array.
+(Array of arrays)*/
+
+/*var arr = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+]
+console.log(arr);*/
+
+/*2. Declare and initialize a multidimensional array
+representing the following matrix:*/
+
+/*var num = [
+    [0,1,2,3],
+    [1,0,1,2],
+    [2,1,0,1]
+];
+for(a = 0; a< 4; a++ ){
+    for(var b = 0; b < 4; b++){
+        document.write(num[a][b] + " ");
+
+    }
+    document.write("<br>");
+}*/
+
+
+// 3. Write a program to print numeric counting from 1 to 10.
+
+/*var numeric = [1,2,3,4,5,6,7,8,9,10];
+
+for(a = 0; a<10; a++){
+
+      document.write(numeric[a] + "<br>");
+}*/
+
+
+/*4. Write a program to print multiplication table of any
+number using for loop. Table number & length should be
+taken as an input from user.*/
+
+/*var msg = prompt("Enter a number to show its multiplication table");
+
+var msg1= prompt("Enter lenght multiplication table");
+
+for(t = 1; t <= 15; t++){
+    // document.write("2 " + "x " + t + "= " + 2*t  + "<br>")
+     document.write(msg  + "x " + t  + "= " + msg*t + "<br>")
+}*/
+
+
+/*5. Write a program to print items of the following array
+using for loop:
+fruits = [“apple”, “banana”, “mango”, “orange”,
+“strawberry”]*/
+
+
+/*6. Generate the following series in your browser. See
+example output.
+a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k*/
+
+
+
+// for(var c = 1; c <15; c++){
+//     document.write( c + "," )
+// }
+
+// for(var r = 10; r > 0; r--){
+//     document.write(r + ",")
+// }
+
+
+// for(var e = 1; e <= 20; e++){
+//     if(e % 2 == 0){
+//         document.write(e + ",");
+//     }
+// }
+
+
+
+
+/*7. You have an array
+A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+Write a program to enable “search by user input” in an
+array.
+After searching, prompt the user whether the given item is
+found in the list or not. Example:*/
+
+// var matchFound = false;
+// var bakery = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
+// var items = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// for(var i = 0; i < items.length; i ++){
+//     if(bakery === "cookie"){
+//         matchFound= true;
+//         alert(bakery + " " + "is available at index 2 in our bakery");
+//         break;  
+//     }
+//     if(matchFound== false) {
+//         alert("We are sorry. " + bakery + " " + "is not available in our bakery");
+//         break;
+  
+//     }
+// }
+
+
+
+// 8. Write a program to identify the largest number in the
+// given array.
+
+
+// var num = [ 24, 53, 78, 91, 12];
+// document.write("Array items: " + num + "<br>");
+
+// var a = num.find(checkLargeNumber)
+
+// document.write("The largest number is " + a);
+
+// function checkLargeNumber(num){
+//     return num > 80;
+
+// }
+
+// 9. Write a program to identify the smallest number in the
+// given array.
+
+
+// var num = [ 24, 53, 78, 91, 12];
+// document.write("Array items: " + num + "<br>");
+
+// var a = num.find(checkSmallNumber)
+
+// document.write("The Smallest number is " + a);
+
+// function checkSmallNumber(num){
+//     return num < 20;
+
+// }
+
+
+/*10. Write a program to print multiples of 5 ranging 1 to
+100.*/
+
+// var num;
+
+// for (i = 5; i <= 100; i+5){
+//     document.write([i]);
+// }
+
+
+
+// string method
+
+
+
+/*1. Write a program that takes two user inputs for first and
+last name using prompt and merge them in a new variable
+titled fullName. Greet the user using his full name.*/
+
+/*var firstName = prompt ("Enter your First Name");
+var lastName =  prompt ("Enter your Last Name");
+var name = (firstName + " " + lastName);
+
+alert("Welcome " + name);*/
+
+
+/*2. Write a program to take a user input about his favorite
+mobile phone model. Find and display the length of user
+input in your browser*/
+ 
+/*var phone = prompt("Whats your Favorite mobile phone model? ");
+var a = phone.lenght;
+
+document.write("My favorite phone is: " + phone + "<br>" + "Lenght of string:" + a);*/
+
+
+/*3. Write a program to find the index of letter “n” in the word
+“Pakistani” and display the result in your browser*/
+
+// var name = ["pakistani"];
+
+// document.write("String: " + name + "<br>" + "Index of 'n'" + name.indexOf("n"));
+
+
+/*4. Write a program to find the last index of letter “l” in the
+word “Hello World” and display the result in your browser.*/
+
+// var a = ["Hello World"];
+
+// document.write("String: " + a + "<br>" + "Last index of 'l' " + a.lastIndexOf("l"));
+
+
+/*5. Write a program to find the character at 3rd index in the
+word “Pakistani” and display the result in your browser.*/
+
+
+// var name = ["pakistani"];
+//  document.write("String: " + name + "<br>" + "Charactor at index 3: " + name.findIndex("3"));
+
+
+/*7. Write a program to replace the “Hyder” to “Islam” in the
+word “Hyderabad” and display the result in your browser.*/
+
+
+
+// var city = "Hyderabad";
+// for(var i = 0; i < text.length; i++) {
+//     if(text.slice(i, i + 5) === "Hyder") {
+//         text = text.slice(0,i) + "Islam" + text.slice(i + 5);
+
+//         document.write("City: " + city + "<br>" + "After replacement: " + text);
+//     }
+    
+// }
+
+ /*10. Write a program that takes user input. Convert and
+ show the input in capital letters.*/
+
+ /*var user = prompt("Enter your input: ");
+
+ user2 = user.toUpperCase();
+
+ document.write("User input: " + user + "<br>" + "Upper case: " + user2);*/
+
+
+ /*11. Write a program that takes user input. Convert and
+show the input in title case.*/
+
+
+/*var input = prompt("Enter your input");
+input2 = input.toTitleCase();
+
+document.write("User input: " + input + "<br>" + "Title case: " + input2);*/
+
+
+
+/*12. Write a program that converts the variable num to
+string.*/
+
+// var num = [35.36];
+
+// num.toString();
+
+// document.write(num);
